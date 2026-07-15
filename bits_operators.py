72eced62fc4650dcf289cb1b2e8217f25a2f5406
harishmallipeddi <167404446+harishmallipeddi@users.noreply.github.com>
2026-07-15 22:06:30 +0530

    Add files via upload

---
+++ bits_operators.py
@@ -0,0 +1,61 @@
+#task 1
+Decimal	 Binary	    ~ (NOT) 	<< 1	<< 2	>> 1	>> 2
+5	     00000101	  -6	    10	    20	    2	    1
+8	     00001000	  -9	    16	    32	    4	    2
+10	     00001010	 -11	    20	    40	    5	    2
+12       00001100	 -13	    24	    48	    6	    3
+15	     00001111	 -16	    30	    60	    7	    3
+20	     00010100	 -21	    40	    80	    10	    5
+25	     00011001	 -26	    50	    100	    12	    6
+32	     00100000	 -33	    64	    128	    16	    8
+
+
+
+
+# Task:2
+# Assignment – Bitwise Operators (Python)
+
+a = 10
+b = 6
+print(a & b)
+
+a = 12
+b = 5
+print(a | b)
+
+a = 15
+b = 9
+print(a ^ b)
+
+a = 20
+print(~a)
+
+a = 8
+print(a << 1)
+
+a = 8
+print(a << 3)
+
+a = 64
+print(a >> 2)
+
+a = 25
+print(a >> 3)
+
+a = 10
+b = 5
+print(a & b)
+print(a | b)
+print(a ^ b)
+
+a = 7
+print(~a)
+print(a << 2)
+print(a >> 1)
+
+#binary represention
+# 5	00000101
+# 9	00001001
+# 12	00001100
+# 18	00010010
+# 25	00011001
